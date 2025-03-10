8cc4dd9b0625ed1fae4c8628fe29ea85865150b8
Tamas Paulin <tamasdavid.paulin@tesco.com>
2025-03-10 17:34:17 +0100

    chore: advance in the course

--- chapter-eight/15_gradient_and_chain_rule.py
+++ chapter-eight/15_gradient_and_chain_rule.py
@@ -1,5 +1,5 @@
 # [Definition] What is the gradient?
-# Answer:  Is a vector composed of all the partial derivatives of a function.
+# Answer:  Is a vector composed of all the partial derivatives of a function with respect to each inputs and weights and bias.
 # f(x,y,z) = 3(x^3)z - y^2 + 5z + 2yz all partial derivation:
 #       |   z9x^2           | x
 #       |   -2y + 2z        | y
@@ -7,11 +7,29 @@
 
 # [Definition] What is the chain rule?
 # Answer:   To improve loss, we need to learn how each weight and bias impacts the loss at the end.
-#           This rule says that the derivative of a function chain is a product of derivatives of all the functions in this chain.
-#           First, we wrote the derivative of the outer function, f(g(x)) , with respect to the inner function, g(x), as this inner function is its parameter.
-#           We repeat this all the way down to the parameter in question!
+#           This rule says that the derivative of a function chain (composite function) is a product of derivatives of all the functions in this chain:
 #           d/dx f(g(x))         ->  df(g(x))/dg(x) * dg(x)/dx                                          ->  f'x(g(x)) * g'x(x)
+#
+#           Partial differentiation with chain rule for X variable:
 #           e/ex f(g(y,h(x,z)))  ->  df(g(y,h(x,z)))/dg(y,h(x,z)) * dg(y,h(x,z))/dh(x,z) * dh(x,z)/dx   -> f'x(g(y,h(x,z))) * g'x(y,h(x,z)) * h'x(x,z)
+#
+#           Explanation:
+#               Imagine the dependency graph of the function f(x,y,z):    f
+#                                                                         |         df/dg
+#                                                                         V
+#                                                                     --- g ---
+#                                                                     |       |     dg/dh
+#                                                                     V       V
+#                                                                     y   -- h --
+#                                                                         |     |   dh/dx
+#                                                                         V     V
+#                                                                         x     z
+#
+#               We want to move through all the branches which terminates at X.
+#               So finally got:
+#                ef   df   dg   dh
+#                -- = -- * -- * --
+#                ex   dg   dh   dx
 
 if __name__ == '__main__':
     print("Function:    h(x) = 3(2x^2)^5")
@@ -31,4 +49,4 @@ if __name__ == '__main__':
 
     print("Function:    h(x) = e^ln(x^2 + 1)")
     print("h(x) = de^ln(x^2+1)/dln(x^2+1) * dln(x^2+1)/dx^2+1 * dx^2+1/dx")
-    print("h'x(x) = (x^2+1) * 1/x^2+1 * 2x") # The simplification/derivative of e^ln(a) is a.
+    print("h'x(x) = (x^2+1) * 1/x^2+1 * 2x")  # The simplification/derivative of e^ln(a) is a.
--- chapter-nine/16_backpropagation.py
+++ chapter-nine/16_backpropagation.py
@@ -1,4 +1,116 @@
+# Manual representation of 1 neuron with 3 inputs in one shot.
+input = [1.0, -2.0, 3.0]
+weights = [- 3.0, -1.0, 2.0]
+bias = 1.0
 
+# Forward pass
+o1 = input[0] * weights[0]
+o2 = input[1] * weights[1]
+o3 = input[2] * weights[2]
+
+# Dot product: neuron output = sum(inputs x weights + bias)
+sum = o1 + o2 + o3 + bias
+
+# Activation function ReLU
+z = max(sum, 0)
+
+# Visualisation graph
+#      (inputs x weights)
+# 1.0  -----|
+#         -3.0 -------
+# -3.0 -----|        |
+#                    |
+# -2.0 -----|        V (sum)    (ReLU)
+#         2.0 -----> 6.0 -----> 6.0 ---->
+# -1.0 -----|        ^  ^
+#                    |  |
+# 3.0 -----|         |  |
+#         6.0 --------  |
+# 2.0 -----|            |
+#   (bias)              |
+# 1.0 -------------------
+
+# Mathematical understanding
+# formula =  ReLU(SUM(inputs x weights) + bias)
+# formula =  ReLU(SUM(multiply(i[0], w[0]), multiply(i[1], w[1]), multiply(i[2], w[2]), bias))
+
+# Backpropagation
+d_value = 1.0  # assume that during backpropagation we got this value to ReLU from the next layer.
+
+# Derivative of ReLU with respect to z and applied chain rule: x > 0 -> d/dx f(x) = x, f'(x) = 1
+d_relu = d_value * (1. if z > 0 else 0.)
+
+# Partial derivative of sum: e/ex f(x,y) = x + y, f'(x, y) = f'(x) = 1
+d_sum_0 = 1
+d_relu_sum_0 = d_relu * d_sum_0
+
+d_sum_1 = 1
+d_relu_sum_1 = d_relu * d_sum_1
+
+d_sum_2 = 1
+d_relu_sum_2 = d_relu * d_sum_2
+
+d_sum_bias = 1
+d_relu_sum_bias = d_relu * d_sum_bias
+
+# Partial derivative of multiplication: f(x,y) = x * y -> f'x(x,y) = 1 * y
+d_mu_we_0 = weights[0]
+d_mu_we_0_d_sum_0 = d_relu_sum_0 * d_mu_we_0
+
+d_mu_in_0 = input[0]
+d_mu_in_0_d_sum_0 = d_relu_sum_0 * d_mu_in_0
+
+d_mu_we_1 = weights[1]
+d_mu_we_1_d_sum_1 = d_relu_sum_1 * d_mu_we_1
+
+d_mu_in_1 = input[1]
+d_mu_in_1_d_sum_1 = d_relu_sum_1 * d_mu_in_1
+
+d_mu_we_2 = weights[2]
+d_mu_we_2_d_sum_2 = d_relu_sum_2 * d_mu_we_2
+
+d_mu_in_2 = input[2]
+d_mu_in_2_d_sum_2 = d_relu_sum_2 * d_mu_in_2
+
+# So the first weights formula (d_mu_we_0_d_sum_0) is (simplified):
+# This is how we calculate the impact of the input to the neuron on the whole function’s output.
+# d_mu_we_0_d_sum_0 = d_relu_sum_0 * d_mu_we_0
+# d_mu_we_0_d_sum_0 = d_relu_sum_0 * weights[0]
+# d_mu_we_0_d_sum_0 = d_relu * d_sum_0 * weights[0]
+# d_mu_we_0_d_sum_0 = d_relu * d_sum_0 * weights[0]
+# d_mu_we_0_d_sum_0 = d_relu * weights[0]
+# d_mu_we_0_d_sum_0 = d_value * (1. if z > 0 else 0.) * weights[0]
+
+# Gradient vectors
+d_input = [d_mu_in_0_d_sum_0, d_mu_in_1_d_sum_1, d_mu_in_2_d_sum_2]
+d_weights = [d_mu_we_0_d_sum_0, d_mu_we_1_d_sum_1, d_mu_we_2_d_sum_2]
+d_bias = d_relu_sum_bias
 
 if __name__ == '__main__':
-    print("Function:    h(x) = 3(2x^2)^5")
+    print("Forward pass result: ", z)
+    print("Initial:             ", d_value)
+    print("ReLU:                ", d_relu)
+    print("Sum:                 ", d_relu_sum_0, d_relu_sum_1, d_relu_sum_2, d_relu_sum_bias)
+    print("Multiplications:     ", d_mu_we_0_d_sum_0, d_mu_in_0_d_sum_0, d_mu_we_1_d_sum_1, d_mu_in_1_d_sum_1, d_mu_we_2_d_sum_2, d_mu_in_2_d_sum_2)
+    print("Gradient vectors:    ", d_input, d_weights, d_bias)
+
+    print("Applying optimization with a negative fraction to the weights to decrease the neuron's output and run forward pass again")
+    print("Weights and bias: ", weights, bias)
+    # Optimize
+    weights[0] += -0.001 * d_weights[0]
+    weights[1] += -0.001 * d_weights[1]
+    weights[2] += -0.001 * d_weights[2]
+    bias += -0.001 * d_bias
+
+    print("Changed weights and bias: ", weights, bias)
+
+    # Next forward pass
+    o1 = input[0] * weights[0]
+    o2 = input[1] * weights[1]
+    o3 = input[2] * weights[2]
+
+    sum = o1 + o2 + o3 + bias
+    z = max(sum, 0)
+
+    print("Optimized result:    ", z)
+
